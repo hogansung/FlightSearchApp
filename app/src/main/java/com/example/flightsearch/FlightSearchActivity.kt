2a829e921a05fbda8ac9d6b43125bea4bb17bdb3
hsung <hogansung1005@gmail.com>
2024-07-04 22:10:06 -0700

    Change top bar color; adjust spaces

--- app/src/main/java/com/example/flightsearch/FlightSearchActivity.kt
+++ app/src/main/java/com/example/flightsearch/FlightSearchActivity.kt
@@ -30,10 +30,12 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -81,10 +83,15 @@ fun HomeScreen(
         topBar = {
             TopAppBar(
                 title = { Text("Flight Search") },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
             )
         }
     ) { innerPadding ->
         Column(
+            verticalArrangement = Arrangement.spacedBy(20.dp),
             modifier = Modifier.padding(innerPadding),
         ) {
             SearchBar(
